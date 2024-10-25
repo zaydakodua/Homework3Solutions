3b510fe38e236c6ddc20c1b0ce9f22a88fdb955c
zaydakodua <156279903+zaydakodua@users.noreply.github.com>
2024-10-25 18:03:00 -0400

    Update templete.js

--- templete.js
+++ templete.js
@@ -10,29 +10,62 @@ penguins.then(function(data) {
     });
 
     // Define the dimensions and margins for the SVG
-
+    const margin = {top: 20, right: 20, bottom: 30, left: 40},
+          width = 960 - margin.left - margin.right,
+          height = 500 - margin.top - margin.bottom;
 
     // Create the SVG container
-    
-    
+    const svg = d3.select("#scatterplot")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
     // Set up scales for x and y axes
-    // d3.min(data, d => d.bill_length_mm)-5
+    const xScale = d3.scaleLinear()
+        .domain([d3.min(data, d => d.PetalLength), d3.max(data, d => d.PetalLength)])
+        .range([0, width]);
+
+    const yScale = d3.scaleLinear()
+        .domain([d3.min(data, d => d.PetalWidth), d3.max(data, d => d.PetalWidth)])
+        .range([height, 0]);
 
     const colorScale = d3.scaleOrdinal()
         .domain(data.map(d => d.Species))
         .range(d3.schemeCategory10);
 
-    // Add scales     
-    
+    // Add scales
+    svg.selectAll("circle")
+        .data(data)
+        .enter().append("circle")
+        .attr("cx", d => xScale(d.PetalLength))
+        .attr("cy", d => yScale(d.PetalWidth))
+        .attr("r", 5)
+        .style("fill", d => colorScale(d.Species));
 
     // Add circles for each data point
-    
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(xScale));
 
     // Add x-axis label
-    
+    svg.append("g")
+        .call(d3.axisLeft(yScale));
 
     // Add y-axis label
-    
+    svg.append("text")
+        .attr("text-anchor", "middle")
+        .attr("x", width / 2)
+        .attr("y", height + margin.bottom)
+        .text("Petal Length");
+
+    svg.append("text")
+        .attr("text-anchor", "middle")
+        .attr("transform", "rotate(-90)")
+        .attr("x", -height / 2)
+        .attr("y", -margin.left + 10)
+        .text("Petal Width");
 
     // Add legend
     const legend = svg.selectAll(".legend")
@@ -41,48 +74,114 @@ penguins.then(function(data) {
         .attr("class", "legend")
         .attr("transform", (d, i) => "translate(0," + i * 20 + ")");
 
-});
+    legend.append("rect")
+        .attr("x", width - 18)
+        .attr("width", 18)
+        .attr("height", 18)
+        .style("fill", colorScale);
 
+    legend.append("text")
+        .attr("x", width - 24)
+        .attr("y", 9)
+        .attr("dy", ".35em")
+        .style("text-anchor", "end")
+        .text(d => d);
+});
 penguins.then(function(data) {
     // Convert string values to numbers
-    
+    data.forEach(function(d) {
+        d.PetalLength = +d.PetalLength;
+        d.PetalWidth = +d.PetalWidth;
+    });
 
     // Define the dimensions and margins for the SVG
-    
+    const margin = {top: 20, right: 20, bottom: 30, left: 40},
+          width = 960 - margin.left - margin.right,
+          height = 500 - margin.top - margin.bottom;
 
     // Create the SVG container
-    
+    const svg = d3.select("#boxplot")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     // Set up scales for x and y axes
-    
+    const xScale = d3.scaleBand()
+        .domain(data.map(d => d.Species))
+        .range([0, width])
+        .padding(0.1);
+
+    const yScale = d3.scaleLinear()
+        .domain([d3.min(data, d => d.PetalLength), d3.max(data, d => d.PetalLength)])
+        .range([height, 0]);
 
     // Add scales     
-    
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(xScale));
+
+    svg.append("g")
+        .call(d3.axisLeft(yScale));
 
     // Add x-axis label
-    
+    svg.append("text")
+        .attr("text-anchor", "middle")
+        .attr("x", width / 2)
+        .attr("y", height + margin.bottom)
+        .text("Species");
 
     // Add y-axis label
-    
+    svg.append("text")
+        .attr("text-anchor", "middle")
+        .attr("transform", "rotate(-90)")
+        .attr("x", -height / 2)
+        .attr("y", -margin.left + 10)
+        .text("Petal Length");
 
+    // Define the rollup function to calculate quartiles
     const rollupFunction = function(groupData) {
         const values = groupData.map(d => d.PetalLength).sort(d3.ascending);
         const q1 = d3.quantile(values, 0.25);
-        return { q1};
+        const median = d3.quantile(values, 0.5);
+        const q3 = d3.quantile(values, 0.75);
+        const iqr = q3 - q1;
+        const min = q1 - 1.5 * iqr;
+        const max = q3 + 1.5 * iqr;
+        return {q1, median, q3, iqr, min, max};
     };
 
     const quartilesBySpecies = d3.rollup(data, rollupFunction, d => d.Species);
 
+    // Add quartiles and boxplot for each species
     quartilesBySpecies.forEach((quartiles, Species) => {
         const x = xScale(Species);
         const boxWidth = xScale.bandwidth();
 
         // Draw vertical lines
-        
-        // Draw box
-        
-        // Draw median line
-        
-        
+        svg.append("line")
+            .attr("x1", x + boxWidth / 2)
+            .attr("x2", x + boxWidth / 2)
+            .attr("y1", yScale(quartiles.min))
+            .attr("y2", yScale(quartiles.max))
+            .attr("stroke", "black");
+
+        // Draw the box
+        svg.append("rect")
+            .attr("x", x)
+            .attr("y", yScale(quartiles.q3))
+            .attr("width", boxWidth)
+            .attr("height", yScale(quartiles.q1) - yScale(quartiles.q3))
+            .attr("fill", "lightblue");
+
+        // Draw the median line
+        svg.append("line")
+            .attr("x1", x)
+            .attr("x2", x + boxWidth)
+            .attr("y1", yScale(quartiles.median))
+            .attr("y2", yScale(quartiles.median))
+            .attr("stroke", "black");
     });
+});
 });
